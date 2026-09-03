aac81838a9e9c0f6caecabc5a8d8cd4da94d7fa7
LUCAS JOSIAS ROCHA COSTA <lucas33607266@edu.df.senac.br>
2026-09-02 21:15:41 -0300

    Refatorando a aplicacao para utilizar templates

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse 
- 
-def home_page(request): 
-    return HttpResponse('<html><title>To-Do lists</title></html>') 
+from django.shortcuts import render
+
+def home_page(request):
+    return render(request, 'home.html')
