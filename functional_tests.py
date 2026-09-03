5f0d7a0f55712e979cab7467f4ef3d1e5e0bf0a5
LUCAS JOSIAS ROCHA COSTA <lucas33607266@edu.df.senac.br>
2026-09-02 21:41:28 -0300

    Melhorando a Pagina Inicial

--- functional_tests.py
+++ functional_tests.py
@@ -41,7 +41,8 @@ class NewVisitorTest(unittest.TestCase):
         # Para múltiplos elementos, usamos find_elements (no plural)
         rows = table.find_elements(By.TAG_NAME, 'tr')  
         self.assertTrue(
-            any(row.text == '1: Estudar testes funcionais' for row in rows)
+            any(row.text == '1: Estudar testes funcionais' for row in rows),
+            "New to-do item did not appear in table"
         )
 
 if __name__ == '__main__':
