04e417e9018586354f662d99a8df65ebc2614827
LUCAS JOSIAS ROCHA COSTA <lucas33607266@edu.df.senac.br>
2026-09-09 19:57:32 -0300

    Testes de unidade para nova view de listas

--- lists/tests.py
+++ lists/tests.py
@@ -14,17 +14,22 @@ class HomePageTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item') 
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        # Atualizamos o redirecionamento para a nova URL!
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-    def test_displays_all_list_items(self):
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
         Item.objects.create(text='item 1')
         Item.objects.create(text='item 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
         self.assertIn('item 1', response.content.decode())
         self.assertIn('item 2', response.content.decode())
-
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
