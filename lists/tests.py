f9b2342a18504ff4ce9c7ef26aea497423e29d7d
LUCAS JOSIAS ROCHA COSTA <lucas33607266@edu.df.senac.br>
2026-09-09 19:36:14 -0300

    Salvando o POST no Banco de Dados

--- lists/tests.py
+++ lists/tests.py
@@ -8,6 +8,12 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        # Verifica se 1 item foi salvo no banco de dados
+        self.assertEqual(Item.objects.count(), 1)  
+        new_item = Item.objects.first()  
+        self.assertEqual(new_item.text, 'A new list item') 
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
